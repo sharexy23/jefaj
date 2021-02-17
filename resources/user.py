9392635116984d50a476a0b5b4bee0ec3f5678ca
sharexy23 <omotietie@yahoo.com>
2021-02-17 02:42:23 +0100

    GHJ

--- resources/user.py
+++ resources/user.py
@@ -7,7 +7,7 @@ from flask_restful import Resource, reqparse
 #from flask_jwt import jwt_required
 from flask_jwt_extended import create_access_token,create_refresh_token,jwt_required
 #from flask_mail import *
-
+from jwt.algorithms import requires_cryptography
 
 class register(Resource):
     parser = reqparse.RequestParser()
